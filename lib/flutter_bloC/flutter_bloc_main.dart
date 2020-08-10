51e381b2af3b73e3ed4fb74a6800e6f8a1d7dd21
adesyudhatama <adesyudhatama@gmail.com>
2020-08-10 19:54:09 +0700

    flutter_bloc_error

--- lib/flutter_bloC/flutter_bloc_main.dart
+++ lib/flutter_bloC/flutter_bloc_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'color_bloc.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     );
   }
@@ -15,7 +17,24 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            backgroundColor: Colors.pink,
+            onPressed: () {},
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            backgroundColor: Colors.purple,
+            onPressed: () {},
+          ),
+        ],
+      ),
       appBar: AppBar(
+        leading: Icon(Icons.adb),
         title: Text(
           "Flutter BloC Demo",
           style: TextStyle(
@@ -29,6 +48,14 @@ class MainPage extends StatelessWidget {
               colors: [Colors.purple, Colors.pink],
             ),
           ),
+        ),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          width: 100,
+          height: 100,
+          color: Colors.pink,
+          duration: Duration(seconds: 1),
         ),
       ),
     );
